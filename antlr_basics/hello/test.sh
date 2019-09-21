4a3f0cfe382a08af7f6a0050c44c3bd9fcad8d61
cposture <cposture@126.com>
2019-09-21 15:11:55 +0800

    fix

--- antlr_basics/hello/test.sh
+++ antlr_basics/hello/test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.7.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 alias grun='java org.antlr.v4.gui.TestRig'
+echo 'generate java code...'
 antlr4 Hello.g4 -o antlr4-runtime-java
 cd antlr4-runtime-java
+echo 'javac *.java...'
 javac *.java
+echo 'done'
 echo 'input hello xxx and Ctrl+D to finish:'
 grun Hello welcome -tokens
 echo 'show gui:'
